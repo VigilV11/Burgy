0147ce11615bb9264f364bee8aa0fe4de2cfee44
VigilV11 <vigilv@pm.me>
2021-03-16 12:31:57 +0530

    feat: dynamically build the burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,8 +1,57 @@
 import React from 'react';
+import styled from 'styled-components';
+import { nanoid } from 'nanoid';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-function Burger() {
-  return <BurgerIngredient />;
+const BurgerImageContainer = styled.div`
+  width: 30%;
+  height: 250px;
+  margin: auto;
+  display: block;
+  overflow: auto;
+
+  /* Laptop screens */
+  @media (min-width: 1000px) and (min-height: 700px) {
+    width: 700px;
+    height: 600px;
+  }
+
+  /* Mobile screens */
+  @media (min-width: 500px) and (min-height: 401px) {
+    width: 450px;
+    height: 400px;
+  }
+
+  /* Portrait mode */
+  @media (min-width: 500px) and (max-height: 400px) {
+    width: 350px;
+    height: 300px;
+  }
+`;
+
+function Burger({ cheeseCount, meatCount, saladCount, baconCount }) {
+  let cheeseItems = [];
+  let meatItems = [];
+  let saladItems = [];
+  let baconItems = [];
+  for (let count = 0; count < cheeseCount; count++)
+    cheeseItems.push(<BurgerIngredient type='cheese' key={nanoid()} />);
+  for (let count = 0; count < meatCount; count++)
+    meatItems.push(<BurgerIngredient type='meat' key={nanoid()} />);
+  for (let count = 0; count < saladCount; count++)
+    saladItems.push(<BurgerIngredient type='salad' key={nanoid()} />);
+  for (let count = 0; count < baconCount; count++)
+    baconItems.push(<BurgerIngredient type='bacon' key={nanoid()} />);
+  return (
+    <BurgerImageContainer>
+      <BurgerIngredient type='bread-top' />
+      {cheeseItems}
+      {meatItems}
+      {saladItems}
+      {baconItems}
+      <BurgerIngredient type='bread-bottom' />
+    </BurgerImageContainer>
+  );
 }
 
 export default Burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,86 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import styled from 'styled-components';
 import Burger from '../../components/Burger/Burger';
 
+const BurgerControlBox = styled.div`
+  width: 80%;
+  /* height: 30%; */
+  margin: auto;
+  margin-top: 3%;
+  background-color: lightseagreen;
+`;
+
+const IngedientBox = styled.div`
+  width: 40%;
+  margin: auto;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const IngredientLabel = styled.h3`
+  display: inline-block;
+  margin-left: 3%;
+  margin-right: 5%;
+  width: 80px;
+`;
+
+const Button = styled.button`
+  width: 30px;
+  height: 20px;
+  display: inline-block;
+  /* text-align: center; */
+  /* margin: 2%; */
+  /* padding: 2%; */
+`;
+
 function BurgerBuilder() {
+  function reducer(state, action) {
+    switch (action) {
+      case 'add':
+        return state + 1;
+      case 'del':
+        return state > 0 ? state - 1 : 0;
+      default:
+        return state;
+    }
+  }
+
+  const [cheeseCount, cheeseDispatch] = useReducer(reducer, 0);
+  const [meatCount, meatDispatch] = useReducer(reducer, 0);
+  const [saladCount, saladDispatch] = useReducer(reducer, 0);
+  const [baconCount, baconDispatch] = useReducer(reducer, 0);
+
   return (
     <>
-      <Burger />
-      <div>Burger Controls</div>
+      <Burger
+        cheeseCount={cheeseCount}
+        meatCount={meatCount}
+        saladCount={saladCount}
+        baconCount={baconCount}
+      />
+      <BurgerControlBox>
+        <IngedientBox>
+          <IngredientLabel>Cheese</IngredientLabel>
+          <Button onClick={() => cheeseDispatch('add')}>+</Button>
+          <Button onClick={() => cheeseDispatch('del')}>-</Button>
+        </IngedientBox>
+        <IngedientBox>
+          <IngredientLabel>Meat</IngredientLabel>
+          <Button onClick={() => meatDispatch('add')}>+</Button>
+          <Button onClick={() => meatDispatch('del')}>-</Button>
+        </IngedientBox>
+        <IngedientBox>
+          <IngredientLabel>Salad</IngredientLabel>
+          <Button onClick={() => saladDispatch('add')}>+</Button>
+          <Button onClick={() => saladDispatch('del')}>-</Button>
+        </IngedientBox>
+        <IngedientBox>
+          <IngredientLabel>Bacon</IngredientLabel>
+          <Button onClick={() => baconDispatch('add')}>+</Button>
+          <Button onClick={() => baconDispatch('del')}>-</Button>
+        </IngedientBox>
+      </BurgerControlBox>
     </>
   );
 }
