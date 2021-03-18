2a46192ad27536e7f9ee3e320b11eb3a079882c6
VigilV11 <vigilv@pm.me>
2021-03-18 15:18:37 +0530

    fix: burger build controls styling

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -5,36 +5,57 @@ import styled from 'styled-components';
 // import IngredientBox from './IngredientBox/IngredientBox';
 
 const BurgerControlBox = styled.div`
-  width: 80%;
+  width: 60%;
+  /* height: 60%; */
+  /* height: 200px; */
   margin: auto;
-  margin-top: 3%;
-  background-color: lightseagreen;
+  margin-top: 5%;
+  background-color: #ccc;
+  border: 0;
+  border-radius: 8px;
+  box-shadow: 0 0 4px 4px rgba(0, 0, 0, 0.2);
 `;
 
 const IngedientBoxContainer = styled.div`
-  width: 40%;
-  margin: auto;
+  width: 60%;
+  height: 30px;
+
+  margin: 3% auto;
+  /* margin-top: 3%; */
   display: flex;
   justify-content: space-evenly;
   align-items: center;
 `;
 
-const IngredientLabel = styled.h3`
+const IngredientLabel = styled.h4`
   display: inline-block;
   margin-left: 3%;
   margin-right: 5%;
   width: 80px;
+  font-weight: 600;
 `;
 
 const Button = styled.button`
-  width: 30px;
-  height: 20px;
+  width: 40px;
+  height: 22px;
   display: inline-block;
+  background-color: orange;
+  border: 0;
+  border-radius: 4px;
+  font-size: 1.2rem;
+  /* text-align: center; */
+  /* font-weight: bolder; */
+
+  &:hover {
+    background-color: orangered;
+  }
 `;
 
 function BuildControls({ allIngredientTypes, dispatch }) {
   return (
     <BurgerControlBox>
+      <p>&nbsp;</p>
+      {/* Dummy for spacing  */}
       {allIngredientTypes.map((ing) => (
         <IngedientBoxContainer key={nanoid()}>
           <IngredientLabel>
@@ -48,6 +69,7 @@ function BuildControls({ allIngredientTypes, dispatch }) {
           </Button>
         </IngedientBoxContainer>
       ))}
+      <p>&nbsp;</p>
     </BurgerControlBox>
   );
 }
